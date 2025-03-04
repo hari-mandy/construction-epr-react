2780a9a981792dafaf4ae68e2ed921a7af4628a5
Hari <hari.mandytech@gmail.com>
2025-03-05 00:14:41 +0530

    functions for the toggle of side menu bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import sitelogo from '../images/dummies-icon.png';
 import notification from '../images/Notifications.png';
 import userprofile from '../images/user-profile.png';
@@ -8,15 +8,60 @@ import DesktopSearchIcon from './DesktopSearchIcon';
 import MobileSearchIcon from './MobileSearchIcon';
 
 const Header = () => {
+    const [width, setWidth] = useState(window.innerWidth);//state to update the window width.
+
+    //useEffect hook to moniter the window width.
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+    }, []);
+
+    //function to toggle the search bar in responsive.
     const openSearchBar = () => {
         document.querySelector('.mobile-search').classList.toggle('open');
+    }
+
+    const menuToggle = () => {
+        const menuButton = document.querySelector(".hamburg-sidemenu");
+        const sideMenu = document.querySelector(".side-menu-bar");
+        const mainContent = document.querySelector(".main-content");
+        const mainTag = document.querySelector("body");
+
+        sideMenu.classList.toggle("collapse-side-menu");
+        mainContent.classList.toggle("expand-main-content");
+
+        // Change icon and add scroll-lock only if screen width is less than 1024px
+        if (width < 1024) {
+            menuButton.classList.toggle("open-menu");
+            mainTag.classList.toggle("scroll-lock");
+        }
+        
+        // Handle window resize for responsive behavior
+        function handleResize() {
+            if (window.innerWidth < 1024) {
+                sideMenu.classList.add("collapse-side-menu");
+                mainContent.classList.add("expand-main-content");
+            } else {
+                sideMenu.classList.remove("collapse-side-menu");
+                mainContent.classList.remove("expand-main-content");
+                menuButton.classList.remove("open-menu"); // Reset icon state
+                mainTag.classList.remove("scroll-lock"); // Remove scroll lock on large screens
+            }
+        }
+
+        // Check on page load and on resize
+        window.addEventListener("resize", function () {
+            if(window.innerWidth >=1024){
+                handleResize();            
+            }
+        });
     }
     
   return (
     <header>
         <div className="site-logo">
             <img src={sitelogo} alt="site Logo"></img>
-            <button className="hamburg-sidemenu">
+            <button className="hamburg-sidemenu" onClick={menuToggle}>
                 <MenuCloseIcon />
                 <MenuOpenIcon />
             </button>
