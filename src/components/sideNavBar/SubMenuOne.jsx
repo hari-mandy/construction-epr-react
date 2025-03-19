db9619c475c4e2dc7c33ad18c9e644db735500a6
Hari <hari.mandytech@gmail.com>
2025-03-19 17:50:02 +0530

    use ref hook added for the active menu link

--- src/components/sideNavBar/SubMenuOne.jsx
+++ src/components/sideNavBar/SubMenuOne.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react"
-import React from 'react'
-import SubMenuTwo from './SubMenuTwo'
+import { useEffect, useState, useRef } from "react";
+import React from "react";
+import SubMenuTwo from "./SubMenuTwo";
 
-const SubMenuOne = ({title, subMenus }) => {
-    const [isExpanded, setIsExpanded] = useState(false)
+const SubMenuOne = ({ title, subMenus }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const ulRef = useRef(null); // Ref for <ul>
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (ulRef.current) {
+                const activeItem = ulRef.current.querySelector(".show-menu");
+                setIsExpanded(!!activeItem);
+            }
+        }, 100);
+    }, []);
 
 
     return (
-        <li className="depth-1 active-depth-1" key={title}>
+        <li className="depth-1" key={title}>
             <h6 onClick={() => setIsExpanded(!isExpanded)}>{title}</h6>
-            <ul className={`side-menu-list-depth-2 ${isExpanded ? "active-depth-2" : ""}`}>
+            <ul ref={ulRef} className={`side-menu-list-depth-2 ${isExpanded ? "active-depth-2" : ""}`}>
                 {subMenus.map((menuItem, index) => (
                     <SubMenuTwo items={menuItem} key={index} />
                 ))}
             </ul>
         </li>
-    )
-}
+    );
+};
 
-export default SubMenuOne
+export default SubMenuOne;
