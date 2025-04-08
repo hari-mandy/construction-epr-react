1ceef3484c208eb12ee9a6d8cbb985cef3387c2e
Hari <hari.mandytech@gmail.com>
2025-04-08 23:51:31 +0530

    profile update with text message

--- src/components/forms/ProfileForm.jsx
+++ src/components/forms/ProfileForm.jsx
@@ -28,9 +28,18 @@ const ProfileForm = () => {
         profile_img:'',
     })
     const oldValue = useRef('');
+    const [successmes, setSuccessMes] = useState('');
     const MAX_FILE_SIZE = 1 * 1024 * 1024; // 1MB
 
     const handleFocus = (event) => { oldValue.current = event.target.value }; //function to compare old & new value.
+
+    const showSuccessMes = () => {
+        setSuccessMes('Details Updated !');
+        setTimeout(() => {
+            setSuccessMes('');
+            window.location.reload();
+        }, 3000);
+    }
 
     const handleChange = async (e, type) => { //function to handle the values.
         if( type === 'profile_img') {
@@ -64,43 +73,47 @@ const ProfileForm = () => {
 
     const handleSubmit= async (e) => {
         e.preventDefault();
-        if(errMessage.name === '' || errMessage.email === '' || errMessage.username === '') {
+        if(errMessage.name === '' && errMessage.email === '' && errMessage.username === '') {
             const result = await fetchPostData('updateuser', userDetail);
             if(result !== 'success') {
                 alert('Details Not updated !')
             }
             const profile = {...JSON.parse(localStorage.getItem('userDetail')), ...userDetail };
             localStorage.setItem('userDetail', JSON.stringify(profile));
-            window.location.reload();
+            showSuccessMes();
             return ;
         }
         return ;
     }
 
     return (
-        <div className='registration-form-container'>
-            <form className="profile-form" onSubmit={handleSubmit}>
-                <div className="registration-form-container">
-                    <div className="form-column">
-                        <InputFile containerStyle="field-profile" inputType="file" name="profile_img" acceptType="" onChange={(e) => handleChange(e,'profile_img')} imgSrc={userDetail.profile_img} errorMessage={errMessage.profile_img}/>
+        <>
+            <div className='registration-form-container-wrapper'>
+                {successmes ? <p className='text-center success-mes'>{successmes}</p> : ''}
+                <form className="profile-form" onSubmit={handleSubmit}>
+                    <div className="registration-form-container">
+                        <div className="form-column">
+                            <InputFile containerStyle="field-profile" inputType="file" name="profile_img" acceptType="" onChange={(e) => handleChange(e,'profile_img')} imgSrc={userDetail.profile_img} errorMessage={errMessage.profile_img}/>
+                        </div>
+                        <div className="form-column">
+                            <InputText labelTitle="First Name" containerStyle="name-field" inputType="text" name="name" value={userDetail.name} onChange={(e) => handleChange(e, 'name')} errorMessage={errMessage.name} onBlurFun={(e) => handleBlur('name', e)} errorMessageAbove="true"/>
+                            <InputText labelTitle="Email" containerStyle="email-field" inputType="email" name="email" value={userDetail.email} onChange={(e) => handleChange(e, 'email')} handleFocus={handleFocus} onBlurFun={(e) => handleBlur('email', e)} errorMessage={errMessage.email} errorMessageAbove="true"/>
+                            <InputText labelTitle="Date Of Birth" containerStyle="date-field" inputType="date" name="dateofbirth" value={userDetail.dateofbirth} onChange={(e) => handleChange(e, 'dateofbirth')}  errorMessageAbove="true" />
+                            <InputText labelTitle="Permanent Address" containerStyle="name-field" inputType="text" name="permanent_address" value={userDetail.permanent_address} onChange={(e) => handleChange(e, 'permanent_address')} errorMessageAbove="true" />
+                            <InputText labelTitle="Postal Code" containerStyle="name-field" inputType="text" name="postal_code" value={userDetail.postal_code} postal_code onChange={(e) => handleChange(e, 'postal_code')} errorMessageAbove="true" />
+                        </div>
+                        <div className="form-column">
+                            <InputText labelTitle="User Name" containerStyle="name-field" inputType="text" name="username" value={userDetail.username} onChange={(e) => handleChange(e, 'username')} onBlurFun={(e)=> handleBlur('username',e)} handleFocus={handleFocus} errorMessage={errMessage.username} errorMessageAbove="true"/>
+                            <InputText labelTitle="Present Address" containerStyle="name-field" inputType="text" name="present_address" value={userDetail.present_address} onChange={(e) => handleChange(e, 'present_address')} errorMessageAbove="true" />
+                            <InputText labelTitle="City" containerStyle="name-field" inputType="text" name="city" value={userDetail.city} onChange={(e) => handleChange(e, 'city')} errorMessageAbove="true" />
+                            <InputText labelTitle="Country" containerStyle="name-field" inputType="text" name="country" value={userDetail.country} onChange={(e) => handleChange(e, 'country')} errorMessageAbove="true"/>
+                        </div>
                     </div>
-                    <div className="form-column">
-                        <InputText labelTitle="First Name" containerStyle="name-field" inputType="text" name="name" value={userDetail.name} onChange={(e) => handleChange(e, 'name')} errorMessage={errMessage.name} onBlurFun={(e) => handleBlur('name', e)} errorMessageAbove="true"/>
-                        <InputText labelTitle="Email" containerStyle="email-field" inputType="email" name="email" value={userDetail.email} onChange={(e) => handleChange(e, 'email')} handleFocus={handleFocus} onBlurFun={(e) => handleBlur('email', e)} errorMessage={errMessage.email} errorMessageAbove="true"/>
-                        <InputText labelTitle="Date Of Birth" containerStyle="date-field" inputType="date" name="dateofbirth" value={userDetail.dateofbirth} onChange={(e) => handleChange(e, 'dateofbirth')}  errorMessageAbove="true" />
-                        <InputText labelTitle="Permanent Address" containerStyle="name-field" inputType="text" name="permanent_address" value={userDetail.permanent_address} onChange={(e) => handleChange(e, 'permanent_address')} errorMessageAbove="true" />
-                        <InputText labelTitle="Postal Code" containerStyle="name-field" inputType="text" name="postal_code" value={userDetail.postal_code} postal_code onChange={(e) => handleChange(e, 'postal_code')} errorMessageAbove="true" />
-                    </div>
-                    <div className="form-column">
-                        <InputText labelTitle="User Name" containerStyle="name-field" inputType="text" name="username" value={userDetail.username} onChange={(e) => handleChange(e, 'username')} onBlurFun={(e)=> handleBlur('username',e)} handleFocus={handleFocus} errorMessage={errMessage.username} errorMessageAbove="true"/>
-                        <InputText labelTitle="Present Address" containerStyle="name-field" inputType="text" name="present_address" value={userDetail.present_address} onChange={(e) => handleChange(e, 'present_address')} errorMessageAbove="true" />
-                        <InputText labelTitle="City" containerStyle="name-field" inputType="text" name="city" value={userDetail.city} onChange={(e) => handleChange(e, 'city')} errorMessageAbove="true" />
-                        <InputText labelTitle="Country" containerStyle="name-field" inputType="text" name="country" value={userDetail.country} onChange={(e) => handleChange(e, 'country')} errorMessageAbove="true"/>
-                    </div>
-                </div>
-                <InputText inputType="submit" value="save" inputStyle="primary-btn" containerStyle="submit-btn-container" />
-            </form>
-        </div>
+                    <InputText inputType="submit" value="save" inputStyle="primary-btn" containerStyle="submit-btn-container" />
+                </form>
+            </div>
+        </>
+
     )
 }
 
